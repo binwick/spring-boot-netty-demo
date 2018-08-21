353604bac92898986eb274d18c44fcf9f02ccfb9
liuruibin <binwick@gmail.com>
2018-08-21 10:49:38 +0800

    config

--- src/main/java/com/fit2cloud/netty/NettyDemoApplication.java
+++ src/main/java/com/fit2cloud/netty/NettyDemoApplication.java
@@ -3,10 +3,8 @@ package com.fit2cloud.netty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
-@EnableWebFlux
 @PropertySource("file:/Users/liuruibin/Dropbox/IDEA/conf/application.properties")
 public class NettyDemoApplication {
 
