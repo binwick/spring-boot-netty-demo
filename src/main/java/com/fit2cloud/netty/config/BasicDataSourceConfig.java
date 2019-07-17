cee1ef93be55f09746c5a76f14af69af03dad91e
liuruibin <binwick@gmail.com>
2019-07-17 10:16:28 +0800

    page helper

--- src/main/java/com/fit2cloud/netty/config/BasicDataSourceConfig.java
+++ src/main/java/com/fit2cloud/netty/config/BasicDataSourceConfig.java
@@ -1,12 +1,34 @@
 package com.fit2cloud.netty.config;
 
+import com.github.pagehelper.PageInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
 
 @Configuration
 @MapperScan(basePackages = "com.fit2cloud.netty.dao", sqlSessionFactoryRef = "sqlSessionFactory")
 @EnableTransactionManagement
 public class BasicDataSourceConfig {
-
+    /**
+     * 分页插件需要自己写，不然无法添加其他的plugins
+     *
+     * @return page helper
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public PageInterceptor pageInterceptor() {
+        PageInterceptor pageInterceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        properties.setProperty("helperDialect", "mysql");
+        properties.setProperty("rowBoundsWithCount", "true");
+        properties.setProperty("reasonable", "true");
+        properties.setProperty("offsetAsPageNum", "true");
+        properties.setProperty("pageSizeZero", "true");
+        pageInterceptor.setProperties(properties);
+        return pageInterceptor;
+    }
 }
