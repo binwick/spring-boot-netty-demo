4d23e5e49d5d66d52869c654d7c04b02bed7158c
liuruibin <binwick@gmail.com>
2019-07-30 16:37:01 +0800

    use mix

--- src/main/java/com/fit2cloud/netty/dao/TagMapper.java
+++ src/main/java/com/fit2cloud/netty/dao/TagMapper.java
@@ -3,16 +3,13 @@ package com.fit2cloud.netty.dao;
 import com.fit2cloud.netty.model.Tag;
 import com.fit2cloud.netty.model.TagExample;
 import org.apache.ibatis.annotations.*;
-import org.apache.ibatis.type.JdbcType;
 
 import java.util.List;
 
 @Mapper
 public interface TagMapper {
-    @SelectProvider(type=TagSqlProvider.class, method="countByExample")
     long countByExample(TagExample example);
 
-    @DeleteProvider(type=TagSqlProvider.class, method="deleteByExample")
     int deleteByExample(TagExample example);
 
     @Delete({
@@ -35,22 +32,8 @@ public interface TagMapper {
     })
     int insert(Tag record);
 
-    @InsertProvider(type=TagSqlProvider.class, method="insertSelective")
     int insertSelective(Tag record);
 
-    @SelectProvider(type=TagSqlProvider.class, method="selectByExample")
-    @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
-        @Result(column="server_id", property="serverId", jdbcType=JdbcType.BIGINT),
-        @Result(column="group_id", property="groupId", jdbcType=JdbcType.BIGINT),
-        @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
-        @Result(column="value", property="value", jdbcType=JdbcType.VARCHAR),
-        @Result(column="cluster_id", property="clusterId", jdbcType=JdbcType.BIGINT),
-        @Result(column="cluster_role_id", property="clusterRoleId", jdbcType=JdbcType.BIGINT),
-        @Result(column="server_name", property="serverName", jdbcType=JdbcType.VARCHAR),
-        @Result(column="cluster_name", property="clusterName", jdbcType=JdbcType.VARCHAR),
-        @Result(column="cluster_role_name", property="clusterRoleName", jdbcType=JdbcType.VARCHAR)
-    })
     List<Tag> selectByExample(TagExample example);
 
     @Select({
@@ -60,27 +43,13 @@ public interface TagMapper {
         "from tag",
         "where id = #{id,jdbcType=BIGINT}"
     })
-    @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
-        @Result(column="server_id", property="serverId", jdbcType=JdbcType.BIGINT),
-        @Result(column="group_id", property="groupId", jdbcType=JdbcType.BIGINT),
-        @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
-        @Result(column="value", property="value", jdbcType=JdbcType.VARCHAR),
-        @Result(column="cluster_id", property="clusterId", jdbcType=JdbcType.BIGINT),
-        @Result(column="cluster_role_id", property="clusterRoleId", jdbcType=JdbcType.BIGINT),
-        @Result(column="server_name", property="serverName", jdbcType=JdbcType.VARCHAR),
-        @Result(column="cluster_name", property="clusterName", jdbcType=JdbcType.VARCHAR),
-        @Result(column="cluster_role_name", property="clusterRoleName", jdbcType=JdbcType.VARCHAR)
-    })
+    @ResultMap("com.fit2cloud.netty.dao.TagMapper.BaseResultMap")
     Tag selectByPrimaryKey(Long id);
 
-    @UpdateProvider(type=TagSqlProvider.class, method="updateByExampleSelective")
     int updateByExampleSelective(@Param("record") Tag record, @Param("example") TagExample example);
 
-    @UpdateProvider(type=TagSqlProvider.class, method="updateByExample")
     int updateByExample(@Param("record") Tag record, @Param("example") TagExample example);
 
-    @UpdateProvider(type=TagSqlProvider.class, method="updateByPrimaryKeySelective")
     int updateByPrimaryKeySelective(Tag record);
 
     @Update({
--- src/main/java/com/fit2cloud/netty/dao/UserMapper.java
+++ src/main/java/com/fit2cloud/netty/dao/UserMapper.java
@@ -3,16 +3,13 @@ package com.fit2cloud.netty.dao;
 import com.fit2cloud.netty.model.User;
 import com.fit2cloud.netty.model.UserExample;
 import org.apache.ibatis.annotations.*;
-import org.apache.ibatis.type.JdbcType;
 
 import java.util.List;
 
 @Mapper
 public interface UserMapper {
-    @SelectProvider(type=UserSqlProvider.class, method="countByExample")
     long countByExample(UserExample example);
 
-    @DeleteProvider(type=UserSqlProvider.class, method="deleteByExample")
     int deleteByExample(UserExample example);
 
     @Delete({
@@ -45,34 +42,8 @@ public interface UserMapper {
     })
     int insert(User record);
 
-    @InsertProvider(type=UserSqlProvider.class, method="insertSelective")
     int insertSelective(User record);
 
-    @SelectProvider(type=UserSqlProvider.class, method="selectByExample")
-    @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
-        @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
-        @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
-        @Result(column="source", property="source", jdbcType=JdbcType.VARCHAR),
-        @Result(column="access_token", property="accessToken", jdbcType=JdbcType.VARCHAR),
-        @Result(column="status", property="status", jdbcType=JdbcType.VARCHAR),
-        @Result(column="created", property="created", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="admin", property="admin", jdbcType=JdbcType.BIT),
-        @Result(column="company", property="company", jdbcType=JdbcType.VARCHAR),
-        @Result(column="phone", property="phone", jdbcType=JdbcType.BIGINT),
-        @Result(column="active_code", property="activeCode", jdbcType=JdbcType.VARCHAR),
-        @Result(column="active_timeout", property="activeTimeout", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="forget_code", property="forgetCode", jdbcType=JdbcType.VARCHAR),
-        @Result(column="forget_timeout", property="forgetTimeout", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="last_login", property="lastLogin", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="description", property="description", jdbcType=JdbcType.VARCHAR),
-        @Result(column="ldap_id", property="ldapId", jdbcType=JdbcType.VARCHAR),
-        @Result(column="staff_id", property="staffId", jdbcType=JdbcType.VARCHAR),
-        @Result(column="last_mode", property="lastMode", jdbcType=JdbcType.VARCHAR),
-        @Result(column="current_group_id", property="currentGroupId", jdbcType=JdbcType.BIGINT),
-        @Result(column="user_type", property="userType", jdbcType=JdbcType.INTEGER),
-        @Result(column="cost_center_id", property="costCenterId", jdbcType=JdbcType.BIGINT)
-    })
     List<User> selectByExample(UserExample example);
 
     @Select({
@@ -83,39 +54,13 @@ public interface UserMapper {
         "from user",
         "where id = #{id,jdbcType=BIGINT}"
     })
-    @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
-        @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
-        @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
-        @Result(column="source", property="source", jdbcType=JdbcType.VARCHAR),
-        @Result(column="access_token", property="accessToken", jdbcType=JdbcType.VARCHAR),
-        @Result(column="status", property="status", jdbcType=JdbcType.VARCHAR),
-        @Result(column="created", property="created", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="admin", property="admin", jdbcType=JdbcType.BIT),
-        @Result(column="company", property="company", jdbcType=JdbcType.VARCHAR),
-        @Result(column="phone", property="phone", jdbcType=JdbcType.BIGINT),
-        @Result(column="active_code", property="activeCode", jdbcType=JdbcType.VARCHAR),
-        @Result(column="active_timeout", property="activeTimeout", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="forget_code", property="forgetCode", jdbcType=JdbcType.VARCHAR),
-        @Result(column="forget_timeout", property="forgetTimeout", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="last_login", property="lastLogin", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="description", property="description", jdbcType=JdbcType.VARCHAR),
-        @Result(column="ldap_id", property="ldapId", jdbcType=JdbcType.VARCHAR),
-        @Result(column="staff_id", property="staffId", jdbcType=JdbcType.VARCHAR),
-        @Result(column="last_mode", property="lastMode", jdbcType=JdbcType.VARCHAR),
-        @Result(column="current_group_id", property="currentGroupId", jdbcType=JdbcType.BIGINT),
-        @Result(column="user_type", property="userType", jdbcType=JdbcType.INTEGER),
-        @Result(column="cost_center_id", property="costCenterId", jdbcType=JdbcType.BIGINT)
-    })
+    @ResultMap("com.fit2cloud.netty.dao.UserMapper.BaseResultMap")
     User selectByPrimaryKey(Long id);
 
-    @UpdateProvider(type=UserSqlProvider.class, method="updateByExampleSelective")
     int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
 
-    @UpdateProvider(type=UserSqlProvider.class, method="updateByExample")
     int updateByExample(@Param("record") User record, @Param("example") UserExample example);
 
-    @UpdateProvider(type=UserSqlProvider.class, method="updateByPrimaryKeySelective")
     int updateByPrimaryKeySelective(User record);
 
     @Update({
